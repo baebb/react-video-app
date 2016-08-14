c2bb4a16cf23cc13dcff34f1826f42e89aef436e
Ross Dyson <dyson.ross@gmail.com>
2016-08-14 14:16:59 -0700

    state set up

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,3 +1,30 @@
-/**
- * Created by Ross on 8/13/2016.
- */
+import React from 'react';
+
+// const SearchBar = () => {
+//     return <input />;
+// };
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''}
+    }
+
+    render() {
+        return (
+            <div>
+                {/*<input onChange={this.onInputChange}/>*/}
+                <input onChange={(event) => this.setState({ term: event.target.value })} />
+                <p>value: {this.state.term}</p>
+            </div>
+        )
+    }
+
+    onInputChange(event) {
+        console.log(this);
+        this.setState({term: event.target.value});
+    }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDkHjG-qwHipI-yFUEAa90SOe8c0MjuGNE';
+
 const App = () => {
-    return <div>hi hi hi</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector(".container"));
